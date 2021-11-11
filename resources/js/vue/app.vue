0df06d49d2f0ed1836c4552c183039dba53416d5
RAakter <rezowana10@gmail.com>
2021-11-11 18:21:36 +0600

    update

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-        Hello
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+        </div>
     </div>
 </template>
 
 <script>
+    import addItemForm
     export default {
 
     }
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,11 +7,11 @@ const mix = require('laravel-mix');
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
+ | file for the application as well as bundling up all the JS vue.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
